ec2985829f21aa829b53e9604b27047b2354c7ba
Rafael Shen <rafael.shen977@gmail.com>
2019-09-30 00:52:14 +0800

    [Text change] basic impl

--- text_change/lib/main.dart
+++ text_change/lib/main.dart
@@ -2,7 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyApp();
+}
+
+class _MyApp extends State<MyApp> {
+  static const _texts = ['Text 1', 'Text 2', 'Text 3'];
+
+  var _textIndex = 0;
+
+  _onTextChangeButtonClicked() {
+    setState(() {
+      _textIndex = (_textIndex + 1) % _texts.length;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,10 +26,10 @@ class MyApp extends StatelessWidget {
           title: Text('Text Changing App'),
         ),
         body: Column(children: [
-          Text("Text 1"),
+          Text(_texts[_textIndex]),
           RaisedButton(
-            child: Text("Change text"),
-            onPressed: null,
+            child: Text("Change Text"),
+            onPressed: _onTextChangeButtonClicked,
           )
         ]),
       ),
