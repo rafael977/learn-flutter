b63ce8956c609ac82ac4fd21ca2c86db03bd9340
Rafael Shen <rafael.shen977@gmail.com>
2019-10-08 16:23:00 +0800

    [8ball] Completed

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  var _ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: Center(
+          child: FlatButton(
+            child: Image.asset('images/ball$_ballNumber.png'),
+            onPressed: () {
+              setState(() {
+                _ballNumber = Random().nextInt(5) + 1;
+              });
+            },
+          ),
+        ));
+  }
+}
